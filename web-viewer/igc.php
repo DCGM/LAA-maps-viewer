1abdd2d81f6e67f899713ea81ab90dd11d4f6216
Jozef Mlich <imlich@fit.vutbr.cz>
2015-04-28 16:35:19 +0200

    added some comments to igc parser

--- web-viewer/igc.php
+++ web-viewer/igc.php
@@ -1,5 +1,8 @@
 <?php
 
+// specification see at
+// http://www.fai.org/gnss-recording-devices/igc-approved-flight-recorders
+
 function DMStoFloat($d, $m, $s, $dir) {
   $dir_val = ($dir == 'N' || $dir == 'E') ? (1.0) : -1.0;
 //echo $d ."°". $m .".". $s . "<br/>";
@@ -10,16 +13,42 @@ function DMStoFloat($d, $m, $s, $dir) {
 function parseIGC($filename) {
   $result = array();
   $data = explode("\n",file_get_contents($filename));
+
   foreach ($data as $line) {
     $type = substr($line, 0, 1);
-    switch ($type) {
-      case 'A':
-//echo "A";
-      break;
-      case 'B':
+    $item = array(
+      'type' => $type
+    );
 
+    switch ($type) {
+      // Single Instance Data Records (A, H, I, J, C, D, G Records)
+      case 'A': //  FR ID NUMBER
+       $item = array(
+         'type' => $type,
+         'manufacturer' => substr($line, 1, 3),
+         'unique_id' => substr($line, 4, 3),
+         'id_extension' => substr($line, 7),
+       );
+        array_push($result, $item);
+      break;
+      case 'H': // FILEHEADER
+          $header[substr($line,0,4)] = substr ($line, 5);
+      break;
+      case 'I': // Extension to the fix (B) record
+      break;
+      case 'J': // Extension to the K record
+      break;
+      case 'C': // Task
+      break;
+      case 'D': // Differential GPS
+      break;
+      case 'G': // Security
+      break;
+
+      // Multiple Instance Data Records (B, E, F, K, L Records)
+      case 'B': // FIX
         $item = array(
-          'type' => 'B',
+          'type' => $type,
           'time' => (substr($line,1,2) * 3600 +  substr($line,3,2) * 60 + substr($line,5,2)),
           'lat' => DMStoFloat(substr($line,7,2), substr($line,9,2).".".substr($line,11,3), 0, substr($line,14,1)),
           'lon' => DMStoFloat(substr($line,15,3), substr($line,18,2).".".substr($line,20,3), 0, substr($line,23,1)),
@@ -29,21 +58,19 @@ function parseIGC($filename) {
           'accuracy' => (strlen($line) > 25) ? substr($line,36,3) : 0,
         );
         array_push($result, $item);
-//echo "B";
       break;
-      case 'E':
-//echo "E";
+      case 'E': // EVENTS
       break;
-      case 'H':
-//echo "H";
+      case 'F': // Satellite constellation
       break;
-      case 'L':
-//echo "L";
+      case 'K': // Data needed less frequently than fixes
       break;
-
+      case 'L': // Log book / comments
+      break;
     }
 //    array_push($result, $line); 
   }
+  echo "<pre>".print_r($header, true)."</pre>";
 
   return $result;
 
