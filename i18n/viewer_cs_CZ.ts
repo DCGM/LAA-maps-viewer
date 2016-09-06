5a89fe0dc55e334ca6c3fcf17de3cc56c72707a1
isirokyadam <isirokyadam@fit.vutbr.cz>
2016-09-06 12:08:20 +0200

    fix viewer CZ translation
    KOR_S > KOR_Z

--- i18n/viewer_cs_CZ.ts
+++ i18n/viewer_cs_CZ.ts
@@ -1272,7 +1272,7 @@ BECAUSE SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY FOR SOFTWARE, 
     <message id="track-list-delegate-section_space_start-short">
         <location filename="../qml/viewer/MyTranslator.qml" line="47"/>
         <source>sws</source>
-        <translation>KOR_S</translation>
+        <translation>KOR_Z</translation>
     </message>
     <message id="track-list-delegate-section_space_end-short">
         <location filename="../qml/viewer/MyTranslator.qml" line="50"/>
