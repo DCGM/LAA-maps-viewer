340ff56102434a9ad7b1e2f28357827354b85805
isirokyadam <isirokyadam@fit.vutbr.cz>
2017-01-08 15:43:20 +0100

    About dialog graphics improvement

--- i18n/viewer_en_US.ts
+++ i18n/viewer_en_US.ts
@@ -939,49 +939,54 @@
         <oldsource>&amp;Cancel</oldsource>
         <translation>Cancel</translation>
     </message>
+    <message id="about-dialog-title">
+        <location filename="../qml/viewer/AboutDialog.qml" line="11"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
     <message id="about-app-title">
-        <location filename="../qml/viewer/AboutDialog.qml" line="16"/>
-        <source>LAA Trajectory Viewer+</source>
-        <oldsource>LAA Trajectory Viewer</oldsource>
+        <location filename="../qml/viewer/AboutDialog.qml" line="17"/>
+        <source>LAA Trajectory Viewer</source>
+        <oldsource>LAA Trajectory Viewer+</oldsource>
         <translation>LAA Trajectory Viewer+ version 1.0</translation>
     </message>
     <message id="about-build-date">
-        <location filename="../qml/viewer/AboutDialog.qml" line="110"/>
+        <location filename="../qml/viewer/AboutDialog.qml" line="139"/>
         <source>Build %1 %2</source>
         <oldsource>Build %1</oldsource>
         <translation>Build %1 %2</translation>
     </message>
     <message id="about-licence">
-        <location filename="../qml/viewer/AboutDialog.qml" line="101"/>
+        <location filename="../qml/viewer/AboutDialog.qml" line="98"/>
         <source>
-Authors: &lt;br/&gt;\n
-Jozef Mlich, Adam Siroky, Pavel Zemcik, &lt;a href=\&quot;http://www.fit.vutbr.cz/\&quot;&gt;FIT VUT Brno&lt;/a&gt; &lt;br/&gt; &lt;br/&gt;\n\n
+                Authors: &lt;br/&gt;\n
+                Jozef Mlich, Adam Siroky, Pavel Zemcik, &lt;a href=\&quot;http://www.fit.vutbr.cz/\&quot;&gt;FIT VUT Brno&lt;/a&gt; &lt;br/&gt; &lt;br/&gt;\n\n
 
-Licence: &lt;br/&gt;\n
+                Licence: &lt;br/&gt;\n
 
-BUT OPEN SOURCE LICENCE
-Version 1.
-Copyright (c) 2013-2015, Brno University of Technology, Antoninska 548/1, 601 90, Czech Republic &lt;br/&gt;
------ &lt;br/&gt;
-BY INSTALLING, COPYING OR OTHER USES OF SOFTWARE YOU ARE DECLARING THAT YOU AGREE WITH THE TERMS AND CONDITIONS OF THIS LICENCE AGREEMENT. IF YOU DO NOT AGREE WITH THE TERMS AND CONDITIONS, DO NOT INSTAL, COPY OR USE THE SOFTWARE.
-IF YOU DO NOT POSESS A VALID LICENCE, YOU ARE NOT AUTHORISED TO INSTAL, COPY OR OTHERWISE USE THE SOTWARE. &lt;br/&gt;
-Definitions: &lt;br/&gt;
-For the purpose of this agreement, Software shall mean a computer program (a group of computer programs functional as a unit) capable of copyright protection and accompanying documentation.
-Work based on Software shall mean a work containing Software or a portion of it, either verbatim or with modifications and/or translated into another language, or a work based on Software. Portions of work not containing a portion of Software or not based on Software are not covered by this definition, if it is capable of independent use and distributed separately.
-Source code shall mean all the source code for all modules of Software, plus any associated interface definition files, plus the scripts used to control compilation and installation of the executable program. Source code distributed with Software need not include anything that is normally distributed (in either source or binary form) with the major components (compiler, kernel, and so on) of the operating system on which the executable program runs.
-Anyone who uses Software becomes User. User shall abide by this licence agreement. &lt;/br&gt;
-BRNO UNIVERSITY OF TECHNOLOGY GRANTS TO USER A LICENCE TO USE SOFTWARE ON THE FOLLOWING TERMS AND CONDITIONS: &lt;br/&gt;
-User may use Software for any purpose, commercial or non-commercial, without a need to pay any licence fee. &lt;br/&gt;
-User may copy and distribute verbatim copies of executable Software with source code as he/she received it, in any medium, provided that User conspicuously and appropriately publishes on each copy an appropriate copyright notice and disclaimer of warranty; keeps intact all the notices that refer to this licence and to the absence of any warranty; and give any other recipients of Software a copy of this licence along with Software. User may charge a fee for the physical act of transferring a copy, and may offer warranty protection in exchange for a fee.
-User may modify his/her copy or copies of Software or any portion of it, thus forming a work based on Software, and copy and distribute such modifications or work, provided that User clearly states this work is modified Software. These modifications or work based on software may be distributed only under the terms of section 2 of this licence agreement, regardless if it is distributed alone or together with other work. Previous sentence does not apply to mere aggregation of another work not based on software with Software (or with a work based on software) on a volume of a storage or distribution medium.
-User shall accompany copies of Software or work based on software in object or executable form with: &lt;br/&gt;
-a) the complete corresponding machine-readable source code, which must be distributed on a medium customarily used for software interchange; or,
-b) written offer, valid for at least three years, to give any third party, for a charge no more than actual cost of physically performing source distribution, a complete machine-readable copy of the corresponding source code, to be distributed on a medium customarily used for software interchange; or,
-c) the information User received as to the offer to distribute corresponding source code. (This alternative is allowed only for noncommercial distribution and only if User received the program in objects code or executable form with such an offer, in accord with subsection b above.)
-User may not copy, modify, grant sublicences or distribute Software in any other way than expressly provided for in this licence agreement. Any other copying, modifying, granting of sublicences or distribution of Software is illegal and will automatically result in termination of the rights granted by this licence. This does not affect rights of third parties acquired in good faith, as long as they abide by the terms and conditions of this licence agreement.
-User may not use and/or distribute Software, if he/she cannot satisfy simultaneously obligations under this licence and any other pertinent obligations.
-User is not responsible for enforcing terms of this agreement by third parties. &lt;br/&gt;
-BECAUSE SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY FOR SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING, BUT PROVIDES SOFTWARE </source>
+                BUT OPEN SOURCE LICENCE
+                Version 1.
+                Copyright (c) 2013-2015, Brno University of Technology, Antoninska 548/1, 601 90, Czech Republic &lt;br/&gt;
+                ----- &lt;br/&gt;
+                BY INSTALLING, COPYING OR OTHER USES OF SOFTWARE YOU ARE DECLARING THAT YOU AGREE WITH THE TERMS AND CONDITIONS OF THIS LICENCE AGREEMENT. IF YOU DO NOT AGREE WITH THE TERMS AND CONDITIONS, DO NOT INSTAL, COPY OR USE THE SOFTWARE.
+                IF YOU DO NOT POSESS A VALID LICENCE, YOU ARE NOT AUTHORISED TO INSTAL, COPY OR OTHERWISE USE THE SOTWARE. &lt;br/&gt;
+                Definitions: &lt;br/&gt;
+                For the purpose of this agreement, Software shall mean a computer program (a group of computer programs functional as a unit) capable of copyright protection and accompanying documentation.
+                Work based on Software shall mean a work containing Software or a portion of it, either verbatim or with modifications and/or translated into another language, or a work based on Software. Portions of work not containing a portion of Software or not based on Software are not covered by this definition, if it is capable of independent use and distributed separately.
+                Source code shall mean all the source code for all modules of Software, plus any associated interface definition files, plus the scripts used to control compilation and installation of the executable program. Source code distributed with Software need not include anything that is normally distributed (in either source or binary form) with the major components (compiler, kernel, and so on) of the operating system on which the executable program runs.
+                Anyone who uses Software becomes User. User shall abide by this licence agreement. &lt;/br&gt;
+                BRNO UNIVERSITY OF TECHNOLOGY GRANTS TO USER A LICENCE TO USE SOFTWARE ON THE FOLLOWING TERMS AND CONDITIONS: &lt;br/&gt;
+                User may use Software for any purpose, commercial or non-commercial, without a need to pay any licence fee. &lt;br/&gt;
+                User may copy and distribute verbatim copies of executable Software with source code as he/she received it, in any medium, provided that User conspicuously and appropriately publishes on each copy an appropriate copyright notice and disclaimer of warranty; keeps intact all the notices that refer to this licence and to the absence of any warranty; and give any other recipients of Software a copy of this licence along with Software. User may charge a fee for the physical act of transferring a copy, and may offer warranty protection in exchange for a fee.
+                User may modify his/her copy or copies of Software or any portion of it, thus forming a work based on Software, and copy and distribute such modifications or work, provided that User clearly states this work is modified Software. These modifications or work based on software may be distributed only under the terms of section 2 of this licence agreement, regardless if it is distributed alone or together with other work. Previous sentence does not apply to mere aggregation of another work not based on software with Software (or with a work based on software) on a volume of a storage or distribution medium.
+                User shall accompany copies of Software or work based on software in object or executable form with: &lt;br/&gt;
+                a) the complete corresponding machine-readable source code, which must be distributed on a medium customarily used for software interchange; or,
+                b) written offer, valid for at least three years, to give any third party, for a charge no more than actual cost of physically performing source distribution, a complete machine-readable copy of the corresponding source code, to be distributed on a medium customarily used for software interchange; or,
+                c) the information User received as to the offer to distribute corresponding source code. (This alternative is allowed only for noncommercial distribution and only if User received the program in objects code or executable form with such an offer, in accord with subsection b above.)
+                User may not copy, modify, grant sublicences or distribute Software in any other way than expressly provided for in this licence agreement. Any other copying, modifying, granting of sublicences or distribution of Software is illegal and will automatically result in termination of the rights granted by this licence. This does not affect rights of third parties acquired in good faith, as long as they abide by the terms and conditions of this licence agreement.
+                User may not use and/or distribute Software, if he/she cannot satisfy simultaneously obligations under this licence and any other pertinent obligations.
+                User is not responsible for enforcing terms of this agreement by third parties. &lt;br/&gt;
+                BECAUSE SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY FOR SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING, BUT PROVIDES SOFTWARE </source>
         <oldsource>
 Authors: &lt;br/&gt;\n
 Jozef Mlich, Adam Siroky, Pavel Zemcik, &lt;a href=\&quot;http://www.fit.vutbr.cz/\&quot;&gt;FIT VUT Brno&lt;/a&gt; &lt;br/&gt; &lt;br/&gt;\n\n
