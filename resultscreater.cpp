9e328b3673368b7f4da5e1384cfb1c92f0bd6c01
isirokyadam <isirokyadam@fit.vutbr.cz>
2017-03-02 12:56:23 +0100

    Fix html start list label

--- resultscreater.cpp
+++ resultscreater.cpp
@@ -212,7 +212,7 @@ void ResultsCreater::createStartListHTML(const QString &filename,
 
     html += "<!DOCTYPE html>\n";
     html += "<html lang=\"en\">\n";
-    html += getHTMLHeader("html-start-list") + "\n";
+    html += getHTMLHeader(getTranslatedString("html-start-list")) + "\n";
 
     html += "<body>\n";
     html += getHTMLBodyScript() + "\n";
