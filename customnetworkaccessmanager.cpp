3230dc1a974c5949f0f9ec8dfd268400d06dd758
Jozef Mlich <jozef.mlich@greycortex.com>
2022-05-13 12:57:32 +0200

    update user agent by version

--- customnetworkaccessmanager.cpp
+++ customnetworkaccessmanager.cpp
@@ -2,5 +2,5 @@
 
 CustomNetworkAccessManager::CustomNetworkAccessManager(QObject *parent) : QNetworkAccessManager(parent)
 {
-    m_userAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0 LaaViewer 0.1+";
+    m_userAgent = QString("Mozilla/5.0 (X11; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0 LaaViewer %1").arg(GIT_VERSION);
 }
