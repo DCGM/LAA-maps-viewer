f00c4ff3871e79dbb9dd878ae6e6f4727cebe1b5
Jozef Mlich <jozef.mlich@greycortex.com>
2022-07-22 08:47:38 +0200

    change user agent

--- src/customnetworkaccessmanager.cpp
+++ src/customnetworkaccessmanager.cpp
@@ -2,5 +2,5 @@
 
 CustomNetworkAccessManager::CustomNetworkAccessManager(QObject *parent) : QNetworkAccessManager(parent)
 {
-    m_userAgent = QString("Mozilla/5.0 (X11; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0 LaaViewer %1").arg(GIT_VERSION);
+    m_userAgent = QString("LaaViewer %1").arg(GIT_VERSION);
 }
