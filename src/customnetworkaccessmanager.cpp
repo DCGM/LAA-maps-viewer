49b07f546db0c5d642c648bc8a1747040032f1ba
Jozef Mlich <jozef.mlich@greycortex.com>
2023-05-05 09:15:26 +0200

    improve agent

--- src/customnetworkaccessmanager.cpp
+++ src/customnetworkaccessmanager.cpp
@@ -1,7 +1,14 @@
+#include <QtGui>
 #include "customnetworkaccessmanager.h"
 
 CustomNetworkAccessManager::CustomNetworkAccessManager(QObject* parent)
     : QNetworkAccessManager(parent)
 {
-    m_userAgent = QString("LaaViewer %1").arg(GIT_VERSION);
+    m_userAgent = QString("Mozilla/5.0 (%1; %2 %3) QtWebEngine/%4 LaaViewer/%5")
+                        .arg(QSysInfo::productType())
+                        .arg(QSysInfo::currentCpuArchitecture())
+                        .arg(QGuiApplication::platformName())
+                        .arg(qVersion())
+                        .arg(GIT_VERSION);
+    qDebug() << m_userAgent;
 }
