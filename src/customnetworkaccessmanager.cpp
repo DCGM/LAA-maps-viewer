65043e61677566be2f3e909becf190a02893c6b7
Jozef Mlich <jozef.mlich@greycortex.com>
2023-05-16 12:53:17 +0200

    fix clang-format

--- src/customnetworkaccessmanager.cpp
+++ src/customnetworkaccessmanager.cpp
@@ -1,14 +1,14 @@
-#include <QtGui>
 #include "customnetworkaccessmanager.h"
+#include <QtGui>
 
 CustomNetworkAccessManager::CustomNetworkAccessManager(QObject* parent)
     : QNetworkAccessManager(parent)
 {
     m_userAgent = QString("Mozilla/5.0 (%1; %2 %3) QtWebEngine/%4 LaaViewer/%5")
-                        .arg(QSysInfo::productType())
-                        .arg(QSysInfo::currentCpuArchitecture())
-                        .arg(QGuiApplication::platformName())
-                        .arg(qVersion())
-                        .arg(GIT_VERSION);
+                      .arg(QSysInfo::productType())
+                      .arg(QSysInfo::currentCpuArchitecture())
+                      .arg(QGuiApplication::platformName())
+                      .arg(qVersion())
+                      .arg(GIT_VERSION);
     qDebug() << m_userAgent;
 }
